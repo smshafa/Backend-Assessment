ba80023ee9b99c80a67c787721946c3c6362bf77
smshafa <s.m.shafaei@outlook.com>
2023-11-13 16:42:43 +0330

    Adding category controller

---
+++ Backend-Assessment/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Backend_Assessment.Application.Features.Query.Category.GetCategories;
+using Backend_Assessment.Application.Features.Query.Product.GetProducts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Assessment.Controllers;
+
+public class CategoryController : ControllerBase
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        
+        public ProductsController(IMediator mediator) => _mediator = mediator;
+        
+        [HttpGet]
+        public async Task<ActionResult> GetCategories()
+        {
+            var products = await _mediator.Send(new GetCategoriesQuery());
+            return Ok(products);
+        }
+    }
+}
